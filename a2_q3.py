c60834adc91cfd93cebc06e5d8c0c2a69a78c867
zza88 <ziyi_zhao@sfu.ca>
2019-06-05 01:14:12 -0700

    Question 3 - Added edge count as part of the metrics

--- a2_q3.py
+++ a2_q3.py
@@ -15,6 +15,12 @@ from csp import CSP, different_values_constraint, backtracking_search
 # ______________________________________________________________________________
 # Erdos-Renyi random graph problem
 
+def edge_count(graph):
+    edge = 0
+    for i in list(graph.keys()):
+        edge += sum(j > i for j in graph[i])
+    return edge
+
 def rand_graph_teaming(graph):
     return CSP(list(graph.keys()),
                    {i: list(graph.keys()) for i in range(0, graph.__len__())},
@@ -27,20 +33,18 @@ def team_count(csp_sol):
 if __name__ == '__main__':
     # graphs = [rand_graph(10, 0.1), rand_graph(10, 0.2), rand_graph(10, 0.3)]
     graphs = [rand_graph(30, 0.1), rand_graph(30, 0.2), rand_graph(30, 0.3),
-              rand_graph(30, 0.4), rand_graph(30, 0.5)]
+              rand_graph(30, 0.4), rand_graph(30, 0.5), rand_graph(30, 0.6),
+              rand_graph(30, 0.7), rand_graph(30, 0.8), rand_graph(30, 0.9)]
     for g in graphs:
         p = rand_graph_teaming(g)
         start_time = time.time()
         res = backtracking_search(p)
         elapsed_time = time.time() - start_time
         if check_teams(g, res):
+            print ("Random graph has " + str(edge_count(g) + " edges"))
             print ("Divided into " + str(team_count(res)) + " teams")
             print ("Problem solved in " + str(elapsed_time) + " seconds")
             print ("Number of assigned  variables: " + str(p.nassigns))
             print ("Number of un-assigned  variables: " + str(len(p.variables) - p.nassigns) + "\n")
         else:
             print ("Result not valid")
-
-    # g = {0: [1, 2], 1: [0], 2: [0], 3: []}
-    # prob = rand_graph_teaming(g)
-    # print(backtracking_search(prob))
